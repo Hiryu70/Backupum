8a089f172f8ed216625a1b496fa4290d20e808f6
Maxim Chervyakov <che.maxv@gmail.com>
2022-07-26 23:04:01 +0200

    fix: git ignore

---
+++ Backupum/Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace Backupum
+{
+    public class Settings
+    {
+        public string[] SourcePaths { get; set; }
+        public string TargetPath { get; set; }
+        public bool Debug { get; set; }
+    }
+
+}
